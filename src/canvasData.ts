37bb9cb072553141055c37cc354aab456b989717
DrHaid <40427630+DrHaid@users.noreply.github.com>
2024-12-09 20:22:00 +0100

    Select custom height map

--- src/canvasData.ts
+++ src/canvasData.ts
@@ -8,16 +8,22 @@ export class CanvasData {
     this.canvas = canvas;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
-    const context = this.canvas.getContext("2d"); 
+    const context = this.canvas.getContext("2d");
     if (context)
       this.ctx = context;
   }
 
   drawImage(src: string): Promise<void> {
-    return new Promise<void>((resolve) => {
+    return new Promise<void>((resolve, reject) => {
       const heightMap = new Image();
       heightMap.src = src;
       heightMap.onload = () => {
+        if (heightMap.width != this.width 
+          || heightMap.height != this.height) {
+            reject(`Wrong image dimensions! Should be ${this.width}x${this.height}.`);
+            return;
+        }
+        
         this.ctx?.drawImage(heightMap,0,0);
         resolve();
       };
--- src/main.ts
+++ src/main.ts
@@ -10,26 +10,47 @@ var heightCanvas: CanvasData;
 var heightMap: HeightMap;
 var stereogram: Stereogram;
 
+const addAlertBanner = (targetParent: HTMLElement, message: string) => {
+  const container = document.createElement("div");
+  container.className = "alert";
+  const text = document.createElement("span");
+  text.textContent = message;
+  const closeButton = document.createElement("span");
+  closeButton.className = "closeButton";
+  closeButton.textContent = "✕";
+  closeButton.onclick = (e) => { (e.target as HTMLElement).parentElement?.remove(); };
+  container.appendChild(text);
+  container.appendChild(closeButton);
+  targetParent.appendChild(container);
+};
+
 const initCanvas = () => {
   const sCanvas = document.getElementById("stereogram") as HTMLCanvasElement;
   stereoCanvas = new CanvasData(sCanvas);
 
   const hCanvas = document.getElementById("height") as HTMLCanvasElement;
   heightCanvas = new CanvasData(hCanvas);
-}
+};
 
-const loadHeightMap = () => {
-  heightCanvas.drawImage(ringHeightMap)
+const loadHeightMap = (src?: string) => {
+  const sourceURL = src ?? ringHeightMap;
+  heightCanvas.drawImage(sourceURL)
     .then(() => {
       heightMap = new HeightMap(heightCanvas)
     }
-  );
+  ).catch((error) => {
+    const container = document.getElementById("heightMapContainer") as HTMLCanvasElement;
+    addAlertBanner(container, error);
+  });
 };
 
 const initInputs = () => {
   const button = document.getElementById("generate") as HTMLButtonElement;
   button.onclick = handleGenerate;
-}
+
+  const input = document.getElementById("heightInput") as HTMLInputElement;
+  input.onchange = handleInputChange;
+};
 
 const handleGenerate = () => {
   const noise = new Noise(stereoCanvas.width / 2, stereoCanvas.height);
@@ -37,7 +58,16 @@ const handleGenerate = () => {
   if (stereoCanvas.ctx){
     stereogram.draw(stereoCanvas.ctx);
   }
-}
+};
+
+const handleInputChange = (input: Event) => {
+  const target = input.target as HTMLInputElement;
+  if (!target.files) return;
+
+  const url = window.URL || window.webkitURL;
+  const src = url.createObjectURL(target.files[0]);
+  loadHeightMap(src);
+};
 
 initInputs();
 initCanvas();
