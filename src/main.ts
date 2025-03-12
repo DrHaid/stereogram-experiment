1ee8e256b2e413b9927662ce66f57f67444974f4
DrHaid <40427630+DrHaid@users.noreply.github.com>
2025-03-12 22:03:24 +0100

    More example height maps and some design changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { Noise } from "./noise";
 import "./style.css"
-import ringHeightMap from "./img/ring_heightmap.png"
 import { CanvasData } from "./canvasData";
 import { HeightMap } from "./heightMap";
 import { Stereogram } from "./stereogram";
+
+import ringHeightMap from "./img/ring_heightmap.png"
+import catHeightMap from "./img/cat_heightmap.png"
+import coneHeightMap from "./img/cone_heightmap.png"
+import spiralHeightMap from "./img/spiral_heightmap.png"
+import noiseHeightMap from "./img/cellnoise_heightmap.png"
+
+const HEIGHT_MAPS = [
+  {name: "Ring", src: ringHeightMap},
+  {name: "Spiral", src: spiralHeightMap},
+  {name: "Cone", src: coneHeightMap},
+  {name: "Cat", src: catHeightMap},
+  {name: "Noise", src: noiseHeightMap}
+]
 
 var stereoCanvas: CanvasData;
 var heightCanvas: CanvasData;
@@ -37,6 +50,7 @@ const loadHeightMap = (src?: string) => {
   heightCanvas.drawImage(sourceURL)
     .then(() => {
       heightMap = new HeightMap(heightCanvas)
+      generateStereogram();
     }
   ).catch((error) => {
     const container = document.getElementById("heightMapContainer") as HTMLCanvasElement;
@@ -46,18 +60,33 @@ const loadHeightMap = (src?: string) => {
 
 const initInputs = () => {
   const button = document.getElementById("generate") as HTMLButtonElement;
-  button.onclick = handleGenerate;
+  button.onclick = generateStereogram;
+
+  const select = document.getElementById("heightSelect") as HTMLSelectElement;
+  select.onchange = handleSelectChange;
+  HEIGHT_MAPS.forEach(map => {
+    const option = document.createElement('option');
+    option.value = map.src;
+    option.textContent = map.name;
+    select.appendChild(option);
+  });
+  select.value = ringHeightMap; // set default heightMap
 
   const input = document.getElementById("heightInput") as HTMLInputElement;
   input.onchange = handleInputChange;
 };
 
-const handleGenerate = () => {
+const generateStereogram = () => {
   const noise = new Noise(stereoCanvas.width / 2, stereoCanvas.height);
   stereogram = new Stereogram(noise, heightMap);
   if (stereoCanvas.ctx){
     stereogram.draw(stereoCanvas.ctx);
   }
+};
+
+const handleSelectChange = (input: Event) => {
+  const target = input.target as HTMLSelectElement;
+  loadHeightMap(target.value);
 };
 
 const handleInputChange = (input: Event) => {
@@ -67,9 +96,16 @@ const handleInputChange = (input: Event) => {
   const url = window.URL || window.webkitURL;
   const src = url.createObjectURL(target.files[0]);
   loadHeightMap(src);
+
+  // add selected file to dropdown
+  const select = document.getElementById("heightSelect") as HTMLSelectElement;
+  const option = document.createElement('option');
+  option.value = src;
+  option.textContent = target.files[0].name;
+  select.appendChild(option);
+  select.value = src;
 };
 
 initInputs();
 initCanvas();
 loadHeightMap();
-
